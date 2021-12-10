d3c112d6b5ac7b9a8eb405076a82ca68e9178779
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2021-12-10 16:30:44 -0500

    Activate aiplatform.googleapis.com

--- cli/commands/cloud.py
+++ cli/commands/cloud.py
@@ -323,6 +323,7 @@ def activate_services(stage, debug=False):
   project_id = stage.project_id_gae
   cmd = (
       f' {GCLOUD} services enable --project={project_id}'
+      f' aiplatform.googleapis.com'
       f' analytics.googleapis.com'
       f' analyticsreporting.googleapis.com'
       f' appengine.googleapis.com'
