620750eec19fefaec951e8dd4ce2c8d44ddc2a98
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2021-12-10 17:01:45 -0500

    Update bq_to_vertex_ai_dataset_creator.py

--- backend/jobs/workers/bigquery/bq_to_vertex_ai_dataset_creator.py
+++ backend/jobs/workers/bigquery/bq_to_vertex_ai_dataset_creator.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from google.cloud import aiplatform
 from jobs.workers.vertexai.vertexai_worker import VertexAIWorker
 
 
