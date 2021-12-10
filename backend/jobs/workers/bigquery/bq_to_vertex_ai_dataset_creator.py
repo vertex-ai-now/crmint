dc690de06148decf0cbf3d8375029e2a838e56a7
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2021-12-10 15:48:54 -0500

    Create bq_to_vertex_ai_dataset_creator.py

---
+++ backend/jobs/workers/bigquery/bq_to_vertex_ai_dataset_creator.py
@@ -0,0 +1,36 @@
+# Copyright 2021 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from google.cloud import aiplatform
+from jobs.workers.vertexai.vertexai_worker import VertexAIWorker
+
+
+class BQToVertexAIDatasetCreator(VertexAIWorker):
+  
+  PARAMS = [
+      ('bq_project_id', 'string', True, '', 'BQ Project ID'),
+      ('bq_dataset_id', 'string', True, '', 'BQ Dataset ID'),
+      ('bq_table_id', 'string', True, '', 'BQ Table ID'),
+  ]
+  
+  def _execute(self):
+    project_id = self._params['bq_project_id']
+    dataset_id = self._params['bq_dataset_id']
+    table_id = self._params['bq_table_id']
+    aiplatform.TabularDataset.create(
+      display_name=f'{project_id}.{dataset_id}.{table_id}', 
+      bq_source=f'bq://{project_id}.{dataset_id}.{table_id}')
+    self.log_info(
+      f'Created Vertex AI tabular dataset from BigQuery named'
+      f' {project_id}.{dataset_id}.{table_id}')
