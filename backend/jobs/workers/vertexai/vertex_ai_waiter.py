27975f361baf25751f11272dce035cad58f898e9
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2021-12-20 22:16:22 -0500

    Create vertex_ai_waiter.py

---
+++ backend/jobs/workers/vertexai/vertex_ai_waiter.py
@@ -0,0 +1,26 @@
+# Copyright 2020 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""CRMint's worker that waits for a Vertex AI job completion."""
+
+
+from jobs.workers.worker import WorkerException
+from jobs.workers.vertexai.vertex_ai_worker import VertexAIWorker
+
+
+class VertexAIWaiter(VertexAIWorker):
+  """Worker that polls job status and respawns itself if the job is not done."""
+
+  def _execute(self):
+    self._enqueue('VertexAIWaiter', {'pipeline_id': self._params['pipeline_id']}, 300)
