4e9dfd26f9478cf26a588ff20110d6195e1aa60d
instant-bqml <78978926+instant-bqml@users.noreply.github.com>
2021-12-21 16:16:44 -0500

    Update vertex_ai_worker.py

--- backend/jobs/workers/vertexai/vertex_ai_worker.py
+++ backend/jobs/workers/vertexai/vertex_ai_worker.py
@@ -23,16 +23,27 @@ from jobs.workers.worker import Worker, WorkerException
 class VertexAIWorker(Worker):
   """Worker that polls job status and respawns itself if the job is not done."""
 
+  def _get_vertexai_job_client(self, location):
+    api_endpoint = f'{location}-aiplatform.googleapis.com'
+    client_options = {'api_endpoint': api_endpoint}
+    return aip.JobServiceClient(client_options=client_options)
+  
   def _get_vertexai_pipeline_client(self, location):
     api_endpoint = f'{location}-aiplatform.googleapis.com'
     client_options = {'api_endpoint': api_endpoint}
     return aip.PipelineServiceClient(client_options=client_options)
+  
+  def _get_batch_prediction_job(self, job_client, job_name):
+    return job_client.get_batch_prediction_job(name=job_name)
   
   def _get_training_pipeline(self, pipeline_client, pipeline_name):
     return pipeline_client.get_training_pipeline(name=pipeline_name)
   
   def _get_location_from_pipeline_name(self, pipeline_name):
     return pipeline_name.split('/')[3]
+  
+  def _get_location_from_job_name(self, job_name):
+    return job_name.split('/')[3]
 
   def _wait_for_pipeline(self, pipeline):
     """Waits for pipeline completion and relays to VertexAIWaiter if it takes too long."""
@@ -41,7 +52,7 @@ class VertexAIWorker(Worker):
     time.sleep(delay)
     while pipeline.state != 'PIPELINE_STATE_SUCCEEDED':
       if waiting_time > 300:  # Once 5 minutes have passed, spawn VertexAIWaiter.
-        self._enqueue('VertexAIWaiter', {'pipeline_id': pipeline.name}, 60)
+        self._enqueue('VertexAIWaiter', {'id': pipeline.name}, 60)
         return
       if delay < 30:
         delay = [5, 10, 15, 20, 30][int(waiting_time / 60)]
@@ -49,3 +60,19 @@ class VertexAIWorker(Worker):
       waiting_time += delay
     if pipeline.state == 'PIPELINE_STATE_FAILED':
       raise WorkerException(f'Training pipeline {pipeline.name} failed.')
+      
+  def _wait_for_job(self, job):
+    """Waits for pipeline completion and relays to VertexAIWaiter if it takes too long."""
+    delay = 5
+    waiting_time = 5
+    time.sleep(delay)
+    while job.state != 'JOB_STATE_SUCCEEDED':
+      if waiting_time > 300:  # Once 5 minutes have passed, spawn VertexAIWaiter.
+        self._enqueue('VertexAIWaiter', {'id': job.name}, 60)
+        return
+      if delay < 30:
+        delay = [5, 10, 15, 20, 30][int(waiting_time / 60)]
+      time.sleep(delay)
+      waiting_time += delay
+    if pipeline.state == 'JOB_STATE_FAILED':
+      raise WorkerException(f'Job {job.name} failed.')
